3351521ea7038c60e9b8a76f345d607c2a68fb8c
TNTrol <sem.go@bk.ru>
2021-11-26 09:53:44 +0300

    add script for data base

---
+++ src/main/java/domain/Role.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class Role {
+    Integer id;
+    String name;
+}
--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package repository;
 
-public class UserRepository {
+import domain.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    User get(Integer id);
+
+    List<User> getAll();
+
+    void update(User user);
+
+    void save(User user);
+
+    void delete(User user);
+
+    User getByLogin(String login);
 }
