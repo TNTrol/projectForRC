51d5d622dd2ed57546e3b696783a306d26c9b13d
TNTrol <sem.go@bk.ru>
2022-02-17 14:36:32 +0300

    add eureka

--- src/main/java/ru/redcollar/store/service/client/MailClient.java
+++ src/main/java/ru/redcollar/store/service/client/MailClient.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.redcollar.store.dto.MailDto;
 
-@FeignClient(name = "MailClient", url = "${mail.address}")
+@FeignClient("notification-service")
 public interface MailClient {
 
-    @PostMapping
+    @PostMapping("/email")
     void sendMail(@RequestBody MailDto mail);
 
 }
