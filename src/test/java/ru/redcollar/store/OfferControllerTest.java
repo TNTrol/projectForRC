cd1bff0c8fcfe8fef5f0452aa795c40049ac92b9
TNTrol <sem.go@bk.ru>
2022-03-10 16:33:56 +0300

    test container

---
+++ src/test/java/ru/redcollar/store/OfferControllerTest.java
@@ -0,0 +1,59 @@
+package ru.redcollar.store;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import java.time.Instant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.redcollar.store.dto.OfferDto;
+import ru.redcollar.store.dto.PackProductDto;
+import ru.redcollar.store.dto.ProductDto;
+import ru.redcollar.store.entity.PackProduct;
+import ru.redcollar.store.entity.StatusOffer;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles(profiles = "testcontainers")
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class OfferControllerTest {
+
+    private final MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @Sql("/db_offer.sql")
+    @Transactional
+    public void successfullyCreationOffer() throws Exception {
+        OfferDto offerDto = new OfferDto();
+        Instant date = Instant.now();
+        offerDto.setDate(date);
+        offerDto.setStatus(StatusOffer.PAID);
+        for(long i = 1; i < 4; ++i){
+            ProductDto productDto = new ProductDto();
+            productDto.setId(i);
+            PackProductDto packProductDto = new PackProductDto();
+            packProductDto.setProduct(productDto);
+            packProductDto.setCount((int) (i * 2));
+            offerDto.getProducts().add(packProductDto);
+        }
+        mockMvc.perform(post("/offer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(offerDto)))
+                .andExpect(status().isOk());
+    }
+}
--- src/test/java/ru/redcollar/store/ProductTest.java
+++ src/test/java/ru/redcollar/store/ProductTest.java
@@ -1,3 +1,5 @@
+package ru.redcollar.store;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
